778113d1108f18dd41fea5c0cd12ee88c37207b7
sonukumarraj007 <sonukumarraj007@gmail.com>
2021-04-17 21:24:44 +0530

    modify-reactive-form-patch-value

--- src/app/component/reactive-form-patch-value/reactive-form-patch-value.component.ts
+++ src/app/component/reactive-form-patch-value/reactive-form-patch-value.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-reactive-form-patch-value',
@@ -6,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveFormPatchValueComponent implements OnInit {
 
-  constructor() { }
+  id: string = "1";
+  userForm: any;
+  topics = ['Angular', 'React', 'Vue'];
+
+  constructor(private formBuilder: FormBuilder, private api: ApiService) { }
 
   ngOnInit(): void {
+    // user form
+    this.userForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      userEmail: ['', Validators.required],
+      phone: ['', Validators.required],
+      topic: ['', Validators.required],
+      timePreference: ['', Validators.required],
+      subscribe: ['', Validators.required],
+    });
+
+    this.api.getSingleUser(this.id).subscribe((res) => {
+      console.log("Single User : ", res)
+
+      this.userForm.patchValue({
+        name: res.name,
+        userEmail: res.userEmail,
+        phone: res.phone,
+        topic: res.topic,
+        timePreference: res.timePreference,
+        subscribe: res.subscribe
+      });
+
+    });
+
+  }
+
+  onSubmit() {
+    console.log(JSON.stringify(this.userForm.value));
+    this.api.updateSingleUser(this.id, this.userForm.value).subscribe((data) => {
+      alert('User Updated Successfully!!');
+    })
   }
 
 }
